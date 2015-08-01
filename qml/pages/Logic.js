90c154f1b5298be604b7aba6d26fd33b38b101f6
Murat Khairulin <murat.khairulin@gmail.com>
2015-08-01 04:59:32 +0300

    Add colors for boxes 10-20

--- qml/pages/Logic.js
+++ qml/pages/Logic.js
@@ -10,7 +10,7 @@ var kBottom = 3
 
 var reversedDirection = [kBottom, kRight, kLeft, kTop]
 
-/*** Colors ***/
-                  /*   1     |    2     |   3      |    4     |   5      |    6     |    7     |    8     |    9    */
-var body_colors = ["#ffff9c", "#ff2421", "#00f3ad", "#298aff", "#dea6ff", "#31eb00", "#ffd2bd", "#9c00f7", "#ffb600" ]
-var text_colors = ["#8b8e00", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ff5500", "#ffffff", "#ffffff" ]
+/*** Colors *** /
+                  /*   1     |    2     |   3      |    4     |   5      |    6     |    7     |    8     |    9     |    10   |   11     |    12     |   13     |   14     |   15     |    16    |    17    |    18    |   19     |   20     */
+var body_colors = ["#ffff9c", "#ff2421", "#00f3ad", "#298aff", "#dea6ff", "#31eb00", "#ffd2bd", "#9c00f7", "#ffb600", "#c5c2c5", "#cefb00", "#ffff00", "#ff187b", "#00d7ef", "#808080", "#2424ff", "#f340ff", "#ffb2b2", "#ffe5a5", "#fe8242" ]
+var text_colors = ["#8b8e00", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ff5500", "#ffffff", "#ffffff", "white",   "#6b7d00", "#7b7900", "white",   "white",   "white",   "white",   "white",   "white",   "#d09400", "white" ]
